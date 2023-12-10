4d92747e029c15a338f9f2229cc131c076b10e63
Ilya <rozhkovich.ilya97@gmail.com>
2023-12-10 19:04:38 +0300

    обавлена новая функция

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def to_uppercase(input_string):
+    """
+     Функция, которая принимает на вход строку и возвращает ее со
+     всеми заглавными буквами.
+    """
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """
+    Функция, которая делает заглавными первые буквы каждого
+    слова в строке, поступившей на вход функции
+    """
+    return input_string.title()
